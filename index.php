b17f0809a2cea6365a328bc27da86d7caff02e1e
Sumon Mia <sumonahamed143.sa@gmail.com>
2019-04-27 01:13:10 +0600

    create link and view img

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>File Download</title>
+</head>
+<body>
+    <img src="upload/tshit.png" alt="" width="100">
+    <a href="index.php?file=tshit.png">download</a>
+</body>
+</html>
