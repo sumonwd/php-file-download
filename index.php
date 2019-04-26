f92367f0c9b4689d3daa5157ce40b6a4e7f81a78
Sumon Mia <sumonahamed143.sa@gmail.com>
2019-04-27 01:25:36 +0600

    completed all process

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
+    // check file
+    if (!empty($_GET['file'])) {
+        $filename = basename($_GET['file']);
+        $pathname = 'upload/' . $filename;
 
+        if (!empty($filename) && file_exists($pathname)) {
+            // define headers
+            header("Cache-Control: public");
+            header("Content-Description: File Transfer");
+            header("Content-Disposition: attachment; filename=$filename");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Emcoding: binary");
+
+            readfile($pathname);
+            exit;
+        }
+    }
 
 ?>
 
